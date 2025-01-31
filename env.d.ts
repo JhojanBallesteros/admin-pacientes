33f38f4089f9246a121b78f0bac179141aa03037
JhojanBallesteros <j_ballesteros@outlook.es>
2025-01-31 01:47:45 -0500

    fix: .env

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv { 
+  VITE_APP_API_KEY: string    
+  VITE_API_AUTH_DOMAIN: string
+  VITE_API_PROJECT_ID: string
+  VITE_API_STORAGE_BUCKET: string
+  VITE_API_MESSAGING_SENDER_ID: string
+  VITE_API_APP_ID: string 
+}
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,6 @@ import 'flatpickr/dist/flatpickr.min.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-console.log("1",process.env.VUE_APP_API_KEY)
 import App from './App.vue'
 import router from './router'
 // Import the functions you need from the SDKs you need
@@ -19,12 +14,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId:process.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_APP_ID,
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId:import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
 };
 
 initializeApp(firebaseConfig);
